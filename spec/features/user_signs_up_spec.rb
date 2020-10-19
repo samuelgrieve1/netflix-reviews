17cc4527680778c01e7937e6258a2c01a25948e0
Ted Francis <tedfrancis@MacBook-Pro.local>
2020-10-19 15:54:05 -0400

    Devise Sign Up

--- spec/features/user_signs_up_spec.rb
+++ spec/features/user_signs_up_spec.rb
@@ -18,6 +18,9 @@ feature 'user registers', %Q{
     fill_in 'Email', with: 'john@example.com'
     fill_in 'Password', with: 'password'
     fill_in 'Password confirmation', with: 'password'
+    fill_in 'Username', with: 'username'
+    fill_in 'First Name', with: 'John'
+    fill_in 'Last Name', with: 'Wick'
 
     click_button 'Sign up'
 
