449b73cfb37cd12eb2c757fbe57272eaec48979a
Maddox Grey <maddoxgrey@Magrathea.fios-router.home>
2020-10-20 16:16:44 -0400

    "Created show page for shows using React"

--- app/controllers/netflix_shows_controller.rb
+++ app/controllers/netflix_shows_controller.rb
@@ -2,4 +2,8 @@ class NetflixShowsController < ApplicationController
   def index
     @netflix_shows = NetflixShow.all
   end
+
+  def show
+    @netflix_show = NetflixShow.find(params[:id])
+  end
 end
