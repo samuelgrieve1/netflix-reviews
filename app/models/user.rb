b4a54a62efda0f611dae927d8f963af6c70536e6
Maddox Grey <maddoxgrey@Magrathea.fios-router.home>
2020-10-21 15:37:03 -0400

    Setting up carrierwave allow users to upload photo

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,11 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  mount_uploader :profile_photo, ProfilePhotoUploader
+  
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
   validates :email, presence: true
   validates :password, presence: true
   validates :username, presence: true
