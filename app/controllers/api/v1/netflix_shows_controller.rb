5078788a6faf14326e7c22aae4bbd6d094a55d38
Ted Francis <tedfrancis@MacBook-Pro.local>
2020-10-21 14:27:57 -0400

    Final code

--- app/controllers/api/v1/netflix_shows_controller.rb
+++ app/controllers/api/v1/netflix_shows_controller.rb
@@ -1,6 +1,5 @@
 class Api::V1::NetflixShowsController < ApplicationController
   def show
-    # render json: { netflix_show: NetflixShow.find(params[:id]) }
     render json: NetflixShow.find(params[:id])
   end
 end
--- spec/controllers/api/v1/netflix_shows_controller_spec.rb
+++ spec/controllers/api/v1/netflix_shows_controller_spec.rb
@@ -6,33 +6,20 @@ RSpec.describe Api::V1::NetflixShowsController, type: :controller do
     genre: "Science Fiction", 
     description: "Humans killing each other with nukes all over the planet!"
   ) }
-  let!(:show2) { NetflixShow.create(
-    title: "The Office", 
-    genre: "Comedy", 
-    description: "Steven Carell ruins a company"
-  ) }
 
   describe "GET#show" do
-
-    it "return a successful status and content type of json" do
-      get :show
+    it "should return the title, genre and description of an individual show on its own show page" do
+      
+      get :show, params: {id: show1.id}
+      returned_response = JSON.parse(response.body)
       
       expect(response.status).to eq 200
       expect(response.content_type).to eq "application/json"
-    end
-
-    it "should return the title, genre and description of an individual show on its own show page" do
-      get :show
-      returned_response = JSON.parse(response.body)
-      binding.pry
-
-      expect(returned_response[0]["title"]).to eq show1.title
-      expect(returned_response[0]["genre"]).to eq show1.genre
-      expect(returned_response[0]["description"]).to eq show1.description
-    
-      expect(returned_response[1]["title"]).to eq show2.title
-      expect(returned_response[1]["genre"]).to eq show2.genre
-      expect(returned_response[1]["description"]).to eq show2.description
+      
+  
+      expect(returned_response["title"]).to eq "The 100"
+      expect(returned_response["genre"]).to eq "Science Fiction"
+      expect(returned_response["description"]).to eq "Humans killing each other with nukes all over the planet!"
     end
   end
 end
