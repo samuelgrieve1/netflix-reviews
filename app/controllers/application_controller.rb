65b18e62a3df6f2803fcdef94eac3b99d10edf0e
samuelgrieve1 <67606820+samuelgrieve1@users.noreply.github.com>
2020-10-23 15:30:07 -0400

    User photos carrierwave2 (#10)
    * Setting up carrierwave allow users to upload photo
    
    * Merge branch 'user_photos_carrierwave' of https://github.com/samuelgrieve1/netflix-reviews into user_photos_carrierwave
    
    * Setup S3 web server
    
    * Profile photo upload feature/testing added
    
    * Added .env.example
    
    Co-authored-by: Maddox Grey <maddoxgrey@Magrathea.fios-router.home>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
   # Prevent CSRF attacks by raising an exception.
 
   protect_from_forgery unless: -> { request.format.json? }
@@ -8,6 +10,6 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :first_name, :last_name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :first_name, :last_name, :profile_photo])
   end
 end
