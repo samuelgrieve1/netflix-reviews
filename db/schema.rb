5e6803f4faea5ad7049e27d5ba3fdfe1abfdbdcf
tedfrancisdev <62035530+tedfrancisdev@users.noreply.github.com>
2020-10-23 14:20:28 -0400

    Review Show Page Final Code (#9)
    * Added new tables and model
    
    * WIP
    
    * Started work on setting up serializers
    
    * - WIP; need to add testing
    
    * Final Code ready for PR
    
    * Edits
    
    Co-authored-by: Ted Francis <tedfrancis@MacBook-Pro.local>
    Co-authored-by: Maddox Grey <maddoxgrey@Magrathea.fios-router.home>

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_19_181336) do
+ActiveRecord::Schema.define(version: 2020_10_21_192455) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,17 @@ ActiveRecord::Schema.define(version: 2020_10_19_181336) do
     t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "netflix_show_id", null: false
+    t.string "comment", null: false
+    t.integer "rating", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["netflix_show_id"], name: "index_reviews_on_netflix_show_id"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
