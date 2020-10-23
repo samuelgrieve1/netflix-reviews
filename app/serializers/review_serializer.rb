270d1b64bfaeb43aa5b8c855091265785f597588
Ted Francis <tedfrancis@MacBook-Pro.local>
2020-10-23 14:14:32 -0400

    Edits

--- app/serializers/review_serializer.rb
+++ app/serializers/review_serializer.rb
@@ -1,9 +1,5 @@
 class ReviewSerializer < ActiveModel::Serializer
   attributes :id, :comment, :rating
 
-  belongs_to :user, if: :current_user?
-
-  def current_user?
-    scope == object.user
-  end
+  belongs_to :user
 end
