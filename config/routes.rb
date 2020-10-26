a3b756a07df51cafe7f405f274411f3b588931d8
Eric Luby <ericluby@ELubyMBP.local>
2020-10-26 14:52:32 -0400

    moved devise statement to top of routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   root 'homes#index'
+  devise_for :users
 
   get '/netflix_shows', to: "homes#index"
   get '/netflix_shows/:id', to: "homes#index"
   get '/users/:id', to: "homes#index"
   
-  devise_for :users
 
   namespace :api do
     namespace :v1 do
