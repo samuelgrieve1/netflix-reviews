b6ff98a0d1cbdb73f08cecadd078d8374bd2fa97
Maddox Grey <35937956+madducks42@users.noreply.github.com>
2020-10-28 14:56:37 -0400

    - Fixed testing issue

--- config/routes.rb
+++ config/routes.rb
@@ -9,11 +9,10 @@ Rails.application.routes.draw do
   
   namespace :api do
     namespace :v1 do
+      resources :users, only: [:show]
+      
       resources :netflix_shows, only: [:index, :show] do
         resources :reviews, only: [:create]
-      end
-      resources :netflix_shows, only: [:index, :show] do
-        resources :users, only: [:show]
       end
     end
   end
